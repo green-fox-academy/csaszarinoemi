8b6a5c04ed1260d8ef27fd3967d2d0fb75bef3f5
CsÃCsászári Noémi <csaszari.noemi@gmail.com>
2018-10-29 17:26:40 +0100

    draw-square

---
+++ Week1/Day3/task22.ts
@@ -0,0 +1,26 @@
+'use strict';
+export{};
+
+let lineCount: number = 6;
+
+// Write a program that draws a square like this:
+//
+// %%%%%%
+// %    %
+// %    %
+// %    %
+// %    %
+// %%%%%%
+//
+// The square should have as many lines as lineCount is
+let x: string = "%"
+let y: string = " "
+for (let a: number = 1; a <= lineCount; a++) {
+    if ( a === 1 ) {
+        console.log(x.repeat(6));
+    } else if ( a > 1 && a < lineCount) {
+        console.log( (x) + (y.repeat(4)) + x );
+    } else  {
+        console.log(x.repeat(6));
+    }
+}
