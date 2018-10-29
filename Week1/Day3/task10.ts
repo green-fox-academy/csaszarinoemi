af760925b40d4ca73ce23b5cde0c20c9406ffc65
CsÃCsászári Noémi <csaszari.noemi@gmail.com>
2018-10-29 11:28:34 +0100

    define-basic-info

---
+++ Week1/Day3/task10.ts
@@ -0,0 +1,7 @@
+'use strict';
+
+let myname: string =("Császári Noémi");
+let myage: number =(31);
+let myheight: number =(1.7);
+let mystatus: boolean =(false);
+console.log(myname, myheight, myage, mystatus);
