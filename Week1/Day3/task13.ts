d157f7b5c4cca234f27c104e4e51d9b0b2efd46b
CsÃCsászári Noémi <csaszari.noemi@gmail.com>
2018-10-29 13:32:20 +0100

    seconds-in-a-day

---
+++ Week1/Day3/task13.ts
@@ -0,0 +1,13 @@
+'use strict';
+
+let currentHours: number = 14;
+let currentMinutes: number = 34;
+let currentSeconds: number = 42;
+
+// Write a program that prints the remaining seconds (as an integer) from a
+// day if the current time is represented by these variables
+
+let a: number=(24-currentHours*60**2);
+let b: number=(60-currentMinutes*60);
+let c: number=(60-currentSeconds);
+console.log(a+b+c);
