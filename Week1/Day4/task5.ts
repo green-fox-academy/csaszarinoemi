b8dcf15c57424ac10903119dd92b5055a543206d
CsÃCsászári Noémi <csaszari.noemi@gmail.com>
2018-10-30 11:40:30 +0100

    Factorial

---
+++ Week1/Day4/task5.ts
@@ -0,0 +1,13 @@
+'use strict';
+export{};
+// -  Create a function called `factorio`
+//    that returns it's input's factorial
+function factorial( x : number) : number {
+    let sum: number = 1;
+    for (let a : number = 1; a <= x; a++) {
+        sum*=a;
+    
+    }
+    return sum;
+}
+console.log(factorial(5));
