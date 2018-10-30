2823f1cd8a79e9b8b9e6ca1ad28a3efa7c2cb901
CsÃCsászári Noémi <csaszari.noemi@gmail.com>
2018-10-30 17:42:22 +0100

    matrix

---
+++ Week1/Day4/task18.ts
@@ -0,0 +1,20 @@
+'use strict';
+export{};
+
+// -  Create (dynamically*) a two dimensional list
+//    with the following matrix**. Use a loop!
+//
+//    0 0 0 1
+//    0 0 1 0
+//    0 1 0 0
+//    1 0 0 0
+//
+// -  Print this two dimensional list to the console
+//
+// * size should depend on a variable
+// ** Relax, a matrix is just like an array
+
+// array.push
+let column: number []=[...i];
+let row: number[] = [...f];
+
