50f13917852a4bde8fedbbbbb5b2300e0bcf3986
CsÃCsászári Noémi <csaszari.noemi@gmail.com>
2018-10-30 13:36:27 +0100

    Compare Length

---
+++ Week1/Day4/task8.ts
@@ -0,0 +1,14 @@
+'use strict'
+export{};
+// -  Create a variable named `firstList`
+//    with the following content: `[1, 2, 3]`
+// -  Create a variable named `secondList`
+//    with the following content: `[4, 5]`
+// -  Log to the console if `secondList` has more elements than `firstList`
+let firstList:number[] = [1, 2, 3];
+let secondList:number[]=[4, 5];
+function comparelist(i:number[], k:number[]) {
+    console.log(i.length < k.length);
+
+   }
+comparelist(firstList,secondList)
