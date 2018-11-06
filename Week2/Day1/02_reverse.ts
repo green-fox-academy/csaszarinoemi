a7a2ea857932e5e01336a4180069cd97407129f4
CsÃCsászári Noémi <csaszari.noemi@gmail.com>
2018-11-06 11:41:21 +0100

    reverse

---
+++ Week2/Day1/02_reverse.ts
@@ -0,0 +1,13 @@
+'use strict';
+export{};
+
+// Create a function that can reverse a string, which is passed as the parameter
+// Use it on this reversed string to check it!
+
+let reversed: string = '.eslaf eb t\'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI';
+function reverse(unreversed:string) {
+  return unreversed.split("").reverse().join("");
+}
+console.log(reverse(reversed));
+
+export = reverse;
