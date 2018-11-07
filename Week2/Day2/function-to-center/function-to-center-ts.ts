0b50bf08af0eead9850789a9f4cfca2d40bebe1b
CsÃCsászári Noémi <csaszari.noemi@gmail.com>
2018-11-07 17:12:05 +0100

    function-to-center

---
+++ Week2/Day2/function-to-center/function-to-center-ts.ts
@@ -0,0 +1,26 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+
+// create a line drawing function that takes 2 parameters:
+// The x and y coordinates of the line's starting point
+// and draws a line from that point to the center of the canvas.
+// Fill the canvas with lines from the edges, every 20 px, to the center.
+
+function center(x, y) {
+  ctx.strokeStyle = "purple";
+  ctx.beginPath();
+  ctx.moveTo(x, y);
+  ctx.lineTo(300, 200);
+  ctx.stroke();
+}
+for (let i = 0; i <= canvas.width; i += 20) {
+  for (let j = 0; j <= canvas.height; j += 20) {
+    if (j === 0 || i === 0 || i === canvas.width || j === canvas.height) {
+      center(i, j);
+    }
+  }
+}
