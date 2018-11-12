4e88a14631a2e621dd955b12871d108301b71d2f
CsÃCsászári Noémi <csaszari.noemi@gmail.com>
2018-11-12 17:00:10 +0100

    ratio + IP Array

--- Week3/Day1/copy-file.ts
+++ Week3/Day1/copy-file.ts
@@ -2,29 +2,29 @@
 // It should take the filenames as parameters
 // It should return a boolean that shows if the copy was successful
 
-export{};
+export { };
 const fs = require('fs');
 
 function readFromFile(fileName: string): string {
   try {
     return fs.readFileSync(fileName, 'utf-8');
-  } catch(e) {
+  } catch (e) {
     return null;
   }
 }
 
 function writeToFile(fileName: string, data: string): void {
   fs.appendFileSync(fileName, data);
-  }
+}
 
 function copyFile(fileFrom, fileTo): boolean {
-let readedString = readFromFile(fileFrom);
-if (readedString !== null) {
-  writeToFile(fileTo, readedString,)
-  return true;
-} else {
-  return false;
-}
+  let readedString = readFromFile(fileFrom);
+  if (readedString !== null) {
+    writeToFile(fileTo, readedString)
+    return true;
+  } else {
+    return false;
+  }
 }
 console.log(copyFile('my-file3.txt', 'my-file.txt'));
 
---
+++ Week3/Day1/logs.ts
@@ -0,0 +1,62 @@
+// Read all data from 'log.txt'.
+// Each line represents a log message from a web server
+// Write a function that returns an array with the unique IP adresses.
+
+export { };
+const fs = require('fs');
+
+function readFromFile(fileName: string): string {
+  try {
+    return fs.readFileSync(fileName, 'utf-8');
+  } catch (e) {
+    console.log(e.message);
+    return null;
+  }
+}
+
+function copyIP(fileName) {
+  let readedString = readFromFile(fileName);
+  let IP: any[] = readedString.split('\r\n');
+  let IPArray: any[] = [];
+  let FinalIPArry: any[] = [];
+  for (let i = 0; i < IP.length; i++) {
+    IPArray.push(IP[i].split(' '));
+    FinalIPArry.push(IPArray[i][8]);
+  }
+  return FinalIPArry
+}
+
+// Write a function that returns the GET / POST request ratio.
+
+let getAndPostArray: any[] = [];
+function getPost(fileName) {
+  let readedString = readFromFile(fileName);
+  let getPostTogether: any[] = readedString.split('\r\n');
+  let getPostArray: any[] = [];
+
+  for (let i = 0; i < getPostTogether.length; i++) {
+    getPostArray.push(getPostTogether[i].split(' '));
+    getAndPostArray.push(getPostArray[i][11]);
+  } return getAndPostArray
+}
+getPost('log.txt');
+
+function getPostLists(Array) {
+  let ratio: number
+  let getArray: any[] = [];
+  let postArray: any[] = [];
+  for (let i = 0; i < Array.length; i++) {
+    if (Array[i] === 'GET') {
+      getArray.push(Array[i]);
+    } else {
+      postArray.push(Array[i]);
+    }
+  } ratio = getArray.length / postArray.length
+
+  console.log(ratio);
+}
+getPostLists(getAndPostArray);
+
+
+
+
