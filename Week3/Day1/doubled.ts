1b11f387534f7cbe55cbd4ea5fb9b395a8f58353
CsÃCsászári Noémi <csaszari.noemi@gmail.com>
2018-11-12 17:58:46 +0100

    dubled

---
+++ Week3/Day1/doubled.ts
@@ -0,0 +1,13 @@
+// Create a method that decrypts duplicated-chars.txt
+
+export { };
+const fs = require('fs');
+
+function readFromFile(fileName: string): string {
+  try {
+    return fs.readFileSync(fileName, 'utf-8');
+  } catch(e) {
+    console.log(e.message);
+    return null;
+  }
+}
