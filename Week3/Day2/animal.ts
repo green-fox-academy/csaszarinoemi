99a3527efb2e2761fa112b7f98c247d6796a1ac5
CsÃCsászári Noémi <csaszari.noemi@gmail.com>
2018-11-14 17:44:00 +0100

    unfinished work

--- Week3/Day2/animal.ts
+++ Week3/Day2/animal.ts
@@ -6,7 +6,7 @@ Every animal can eat() which decreases their hunger by one
 Every animal can drink() which decreases their thirst by one
 Every animal can play() which increases both by one */
 
-class Animal {
+ class Animal {
   hungerValue = 50;
   thirstValue = 50;
 
@@ -24,6 +24,19 @@ class Animal {
 
 let moa = new Animal();
 let giraffe = new Animal();
+let kiwi = new Animal();
+let emu = new Animal();
+let elephant = new Animal();
+
+
 
 moa.play();
+giraffe.drink();
+giraffe.drink();
+giraffe.drink();
+elephant.drink();
+elephant.drink();
+
 console.log(moa.thirstValue);
+
+export{Animal, moa, giraffe, kiwi, emu, elephant};
