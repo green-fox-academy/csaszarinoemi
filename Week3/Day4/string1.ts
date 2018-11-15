7ae6c3b59b1739693c2c41fac1545d9fcf2cba2c
CsÃCsászári Noémi <csaszari.noemi@gmail.com>
2018-11-15 14:36:18 +0100

    Change x to y. Texas with recursion

---
+++ Week3/Day4/string1.ts
@@ -0,0 +1,13 @@
+// Given a string, compute recursively (no loops) a new string where all the
+// lowercase 'x' chars have been changed to 'y' chars.
+
+function changeLowercaseXToLetterY(textIn:string) {
+
+  if (textIn.replace('x', 'y') === textIn ){
+    return textIn;
+  } else {
+    return changeLowercaseXToLetterY(textIn.replace('x', 'y'));
+  }
+}
+
+console.log(changeLowercaseXToLetterY("TexxxxxxXas"));
