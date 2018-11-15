ac02bd9eb46ac1b4af9b354b6e8bfc3bcd7ce6a8
CsÃCsászári Noémi <csaszari.noemi@gmail.com>
2018-11-15 16:14:53 +0100

    factorial

---
+++ Week3/Day4/refactorio.ts
@@ -0,0 +1,13 @@
+// Create a recursive function called `refactorio`
+// that returns it's input's factorial
+
+function factorial(num: number): number {
+  if (num <= 1) { //base case
+    return 1;
+  }
+  else {
+    return num * factorial(num - 1);
+  }
+}
+
+console.log('5! is', factorial(5));
