0c1f7cadde1c98be3e4f5514fd20a356219e0ed8
CsÃCsászári Noémi <csaszari.noemi@gmail.com>
2018-11-19 14:10:39 +0100

    (feat garden): add garden class

---
+++ Week4/Day1/garden/garden.ts
@@ -0,0 +1,15 @@
+class Garden {
+  flower: number;
+  tree: number;
+
+  constructor(flower: number, tree: number) {
+    this.flower = flower;
+    this.tree = tree;
+  }
+
+  watering(element){
+    if (element.waterlevel < 5) {
+
+    }
+  }
+}
