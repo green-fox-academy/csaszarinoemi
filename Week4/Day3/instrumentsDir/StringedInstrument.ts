b0beefe2f4661589eb52b784d56573647a511e35
CsÃCsászári Noémi <csaszari.noemi@gmail.com>
2018-11-21 12:00:13 +0100

    feat (InstrumentDir): add classes

---
+++ Week4/Day3/instrumentsDir/StringedInstrument.ts
@@ -0,0 +1,14 @@
+import { Instrument } from "./instrument";
+
+
+export abstract class StringedInstrument extends Instrument {
+  numberOfStrings: number;
+
+  constructor(numberOfStrings: number){
+    super(name);
+    this.numberOfStrings = numberOfStrings;
+  }
+  sound() {
+
+  }
+}
