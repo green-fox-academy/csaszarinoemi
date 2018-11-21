30637cdb5323726c1ee16581f0a6e15a48163ede
CsÃCsászári Noémi <csaszari.noemi@gmail.com>
2018-11-21 14:05:24 +0100

    feat(Animal): add Animal class

---
+++ Week4/Day3/zoo/animal.ts
@@ -0,0 +1,23 @@
+
+
+export abstract class Animal {
+  name: string;
+  age: number;
+  gender: string;
+  type: string;
+
+  constructor(name: string, age: number, gender: string, type: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+    this.type = type;
+  }
+  
+  getName(){
+
+  }
+
+  breed(){
+
+  }
+}
