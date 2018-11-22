6f1d3c472455a21bd7b9939b86def795641ae3c8
CsÃCsászári Noémi <csaszari.noemi@gmail.com>
2018-11-22 15:02:06 +0100

    feat (fibonacci): throw error testing

--- Week4/Day4/fibonacci/test.ts
+++ Week4/Day4/fibonacci/test.ts
@@ -4,8 +4,10 @@ import { fibonacci } from './fibonacci';
 test('Testing fibonacci with numbers', t => {
   t.plan(5);
   t.equal(fibonacci(0), 0);
-  t.throws(fibonacci(-1));
+  t.throws(function(){
+    fibonacci(-1)});
   t.equal(fibonacci(1), 1);
   t.equal(fibonacci(7), 13);
-  t.throws(fibonacci(1.5));
+  t.throws(function(){
+    fibonacci(1.5)});
 })
