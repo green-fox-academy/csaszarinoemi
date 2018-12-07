b9ab36d99eab58641d30f9657879bc874bbb724c
CsÃCsászári Noémi <csaszari.noemi@gmail.com>
2018-12-07 11:24:10 +0100

    feat(gallery): add html, js, css

---
+++ Week6/Day3/main.js
@@ -0,0 +1,12 @@
+'use strict'
+
+let myPics = [
+  http://www.travelways.com.sg/uploads/tours/images/19/south-island-new-zealand-landscape-reflection-river-forest-fog-mist-high-resolution-images_1450845998.jpg,
+  https://blog.backpackerdeals.com/wp-content/uploads/2013/07/jon-arnold-southern-alps-south-island-new-zealand-road-scenery-hd-nature.jpg,
+  https://dak95nwic4sny.cloudfront.net/73/new-zealand-30876048-1536579250-ImageGalleryLightboxLarge.jpg,
+  https://wallpapershome.com/images/wallpapers/lake-tekapo-1920x1080-5k-4k-wallpaper-south-island-new-zealand-566.jpg,
+  https://www.arohatours.co.nz/media/1431/rainforest.jpg,
+  https://endlessloopphotography.files.wordpress.com/2017/03/tasman-glacier.jpg,
+  http://www.reachcoach.co.nz/en/wp-content/uploads/2016/04/21BEBBD4-9720-DEA9-7B63219DAAFD3260.jpg,
+  https://cdn.cnn.com/cnnnext/dam/assets/180518103108-aurora-australis-new-zealand---milky-way.jpg,
+]
