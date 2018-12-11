19dce4e2a4192c57ee4ecf0df7d32ee28fb7047a
CsÃCsászári Noémi <csaszari.noemi@gmail.com>
2018-12-11 14:45:41 +0100

    feat(giphy): styleguide

--- Week6/Day4/giphy.ts
+++ Week6/Day4/giphy.ts
@@ -5,18 +5,18 @@ httpRequest.open('GET', 'http://api.giphy.com/v1/gifs/search?q=wow&api_key=PJKcR
 httpRequest.send();
 
 httpRequest.onreadystatechange = () => {
-    if (httpRequest.readyState === XMLHttpRequest.DONE) {
-      if (httpRequest.status === 200) {
-        const response = JSON.parse(httpRequest.responseText);
-        const myBody = document.querySelector('body');
-        for (let i = 0; i < response.data.length; i++){
+  if (httpRequest.readyState === XMLHttpRequest.DONE) {
+    if (httpRequest.status === 200) {
+      const response = JSON.parse(httpRequest.responseText);
+      const myBody = document.querySelector('body');
+      for (let i = 0; i < response.data.length; i++) {
         let imageIWillSee = document.createElement('img')
         myBody.appendChild(imageIWillSee);
-        imageIWillSee.setAttribute('src',(response.data[i].images.original_still.url));
-        imageIWillSee.onclick = () =>{
-          imageIWillSee.setAttribute('src',(response.data[i].images.original.url));
-        }
+        imageIWillSee.setAttribute('src', (response.data[i].images.original_still.url));
+        imageIWillSee.onclick = () => {
+          imageIWillSee.setAttribute('src', (response.data[i].images.original.url));
         }
       }
     }
-  };
+  }
+};
