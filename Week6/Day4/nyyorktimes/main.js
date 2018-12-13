1096764d06ca3b811ab1e22e550e7e484fa545e2
CsÃCsászári Noémi <csaszari.noemi@gmail.com>
2018-12-13 19:42:33 +0100

    feat(newyorktimes): add exception

--- Week6/Day4/nyyorktimes/main.js
+++ Week6/Day4/nyyorktimes/main.js
@@ -1,45 +1,48 @@
 'use strict'
 
 
-
 let httpRequest = new XMLHttpRequest();
 httpRequest.open('GET', "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=moon&landing&apollo&11&api_key=e1584a15933e40f0b87d8d377fadf2ce");
 httpRequest.send();
 
 httpRequest.onreadystatechange = () => {
+
   if (httpRequest.readyState === XMLHttpRequest.DONE) {
+
     if (httpRequest.status === 200) {
+
       const myResponse = JSON.parse(httpRequest.responseText).response.docs;
       const myBody = document.querySelector('body');
+      console.log(myResponse);
 
-      for (let i = 0; i < myResponse.length; i++){
+      for (let i = 0; i < myResponse.length; i++) {
 
-        let myUl = document.createElement('ul');
-        let headline = document.createElement('li');
-        let snippet = document.createElement('li');
-        let publicationDate = document.createElement('li');
+        if (myResponse[i].document_type !== 'topic') {
 
-        myBody.appendChild(myUl);
-        myUl.appendChild(headline);
-        myUl.appendChild(snippet);
-        myUl.appendChild(publicationDate);
-        
-         
-        let myATag = document.createElement('a');
-        myATag.setAttribute('href', myResponse[i].web_url)
-        myATag.innerText = myResponse[i].headline.main;
+          let myUl = document.createElement('ul');
+          let headline = document.createElement('li');
+          let snippet = document.createElement('li');
+          let publicationDate = document.createElement('li');
 
-        headline.appendChild(myATag);
-        
-        snippet.innerText = myResponse[i].snippet;
+          myBody.appendChild(myUl);
+          myUl.appendChild(headline);
+          myUl.appendChild(snippet);
+          myUl.appendChild(publicationDate);
 
-        if (typeof myResponse[i].pub_date !== 'undefined'){
-          publicationDate.innerText = myResponse[i].pub_date.slice(0,10);
-        } else {
-          publicationDate.innerText = myResponse[i].pub_date
+
+
+          let myATag = document.createElement('a');
+          myATag.setAttribute('href', myResponse[i].web_url)
+          myATag.innerText = myResponse[i].headline.main;
+
+          headline.appendChild(myATag);
+
+          snippet.innerText = myResponse[i].snippet;
+
+          publicationDate.innerText = myResponse[i].pub_date.slice(0, 10);
         }
-        }
-     
+      }
+
     }
   }
-};
+}
