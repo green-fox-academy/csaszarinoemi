536252610be4b2d50b57ea5f6ba76d4b3dd6f8dc
CsÃCsászári Noémi <csaszari.noemi@gmail.com>
2018-12-19 12:00:29 +0100

    feat(app.js): add doubling end

--- Week7/Day3/frontend/app.js
+++ Week7/Day3/frontend/app.js
@@ -1,12 +1,27 @@
 const express = require('express')
 const app = express();
+const path = require('path');
 
-const PORT = 3000;
+const PORT = 8080;
+
+app.use('/assets', express.static('assets'));
 
 app.get('/', (req, res) => {
-  res.send('OK!');
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/doubling', (req, res) => {
+  const { input } = req.query;
+  if (input) {
+    res.json({
+      'received': input,
+      'result': input * 2
+    });
+  } else {
+    res.json({ 'error': 'Please provide an input!' });
+  }
 });
 
 app.listen(PORT, () => {
-  console.log('Application is listening on port 3000');
+  console.log('Application is listening on port 8080');
 })
