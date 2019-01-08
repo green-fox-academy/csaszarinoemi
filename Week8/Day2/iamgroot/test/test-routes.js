865790a125d9b26e46213130350d9db1e23d3919
CsÃCsászári Noémi <csaszari.noemi@gmail.com>
2019-01-08 11:45:53 +0100

    feat(iAmGroot): add test cases

--- Week8/Day2/iamgroot/test/test-routes.js
+++ Week8/Day2/iamgroot/test/test-routes.js
@@ -1,9 +1,43 @@
 'use strict';
-​
+
 const test = require('tape');
 const request = require('supertest');
 const app = require('../routes');
-​
-test('groot endpoint', (t) => {
-  // TODO: implement it 
+
+test('groot endpoint WITHOUT QUERY', (t) => {
+  request(app)
+    .get('/groot')
+    .expect('Content-Type', /json/)
+    .expect(500)
+    .end((err, res) => {
+      if (err) throw err;
+      t.same(res.body, {error: 'I am Groot!'}, 'without query test');
+      t.end();
+    });
+});
+
+test('TESTING GET / ENDPOINT WITH QUERY', (t) => {
+  request(app)
+    .get('/groot/?message=valami')
+    .expect(200)
+    .expect('Content-Type', /json/)
+    .end((err, res) => {
+      if (err) throw err;
+      t.same(res.body, {
+        "received": "valami",
+      "translated": "I am Groot!"
+    }, 'with query test');
+      t.end();
+    });
+});
+
+test('TESTING GET / ENDPOINT WITH QUERY', (t) => {
+  request(app)
+    .get('/groot/?valami=valami')
+    .expect(500)
+    .end((err, res) => {
+      if (err) throw err;
+      t.same(res.body, {error: 'I am Groot!'}, 'without query test');
+      t.end();
+    });
 });
