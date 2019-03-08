c916062db49ccc4c9c0a8d70ad970c43f715ea8a
Noemi Csaszari <csaszari.noemi@gmail.com>
2019-03-08 15:42:35 +0100

    refactor

--- Week1/Day3/task10.ts
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-let myname: string =("Császári Noémi");
-let myage: number =(31);
-let myheight: number =(1.7);
-let mystatus: boolean =(false);
-console.log(myname, myheight, myage, mystatus);
---
+++ Week1/Day3/task10_.define_basic_infos.ts
@@ -0,0 +1,14 @@
+'use strict';
+
+
+// Define several things as a variable, then print their values
+// Your name as a string
+// Your age as a number
+// Your height in meters as a number
+// Whether you are married or not as a boolean
+
+let myname: string =("Császári Noémi");
+let myage: number =(31);
+let myheight: number =(1.7);
+let mystatus: boolean =(false);
+console.log(myname, myheight, myage, mystatus);
